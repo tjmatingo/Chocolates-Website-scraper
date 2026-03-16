f69756207cf6e5a296f1c6d64d064e91e5cc6256
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-16 20:48:22 +0200

    scraping done from chocolates website as well delivered into csv and json fiels

--- chocolatescraper/chocolatescraper/spiders/chocolatespider.py
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -3,8 +3,24 @@ import scrapy
 
 class ChocolatespiderSpider(scrapy.Spider):
     name = "chocolatespider"
-    allowed_domains = ["chocolates.co.uk"]
-    start_urls = ["https://chocolates.co.uk"]
+    allowed_domains = ["chocolate.co.uk"]
+    start_urls = ["https://www.chocolate.co.uk/collections/all-products"]
 
     def parse(self, response):
-        pass
+        products = response.css('product-item')
+
+        # format for data scraped into a dictionary
+        for product in products:
+            yield {
+                'name': product.css('a.product-item-meta__title::text').get(),
+                "price": product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>', '').replace('\n</span>', '').replace('</span>', '').replace('<span class=\"price price--highlight\">\n              <span class=\"visually-hidden\">', '').replace('From', '').replace('Sale price', ''),
+                'url': product.css('div.product-item-meta a').attrib['href'],
+            }
+
+        # for scraping from all pages using pagination
+
+        next_page = response.css('[rel="next"] ::attr(href)').get()
+
+        if next_page is not None:
+            next_page_url = "https://www.chocolate.co.uk" + next_page 
+            yield response.follow(next_page_url, callback=self.parse)
