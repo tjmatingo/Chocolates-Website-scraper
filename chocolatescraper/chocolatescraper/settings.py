ca131c38b9f042ec413653fb07ca19c64a846391
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-20 10:56:12 +0200

    user agents setup

--- chocolatescraper/chocolatescraper/settings.py
+++ chocolatescraper/chocolatescraper/settings.py
@@ -46,9 +46,11 @@ DOWNLOAD_DELAY = 1
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "chocolatescraper.middlewares.ChocolatescraperDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   # "chocolatescraper.middlewares.ChocolatescraperDownloaderMiddleware": 543,
+   'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+   'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
