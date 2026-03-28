dbaf1da5ebe68428775d917bbb94cb88f5bba2bd
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-28 22:41:35 +0200

    product data parsing completed

--- scrapyAmazonProducts/basic-scrapy-project/basic_scrapy_spider/spiders/quotes.py
+++ scrapyAmazonProducts/basic-scrapy-project/basic_scrapy_spider/spiders/quotes.py
@@ -38,3 +38,15 @@ class AmazonSearchProductSpider(scrapy.Spider):
         image_data = json.loads(re.findall(r"colorImages':.*initial':\s*(\[.+?\])},\n", response.text)[0])
         variant_data = re.findall(r'dimensionValuesDisplayData"\s*:\s* ({.+?}),\n', response.text)
         feature_bullets = [bullet.strip() for bullet in response.css("#feature-bullets li ::text").get*()]
+        price = response.css('.a-price span[aria-hidden="true"] ::text').get("")
+        if not price:
+            price = response.css('.a-price .a-offscreen ::text').get("")
+            yield {
+                "name": response.css("#productTitle::text").get("").strip(),
+                'price': price,
+                "stars": response.css["i[data-hook=average-star-rating] ::text"].get("").strip(),
+                "rating_count": response.css('div[data-hook=total-review-count] ::text').get("").strip(),
+                "feature_bullets": feature_bullets,
+                'images': image_data,
+                "variant_data": variant_data,
+            }
