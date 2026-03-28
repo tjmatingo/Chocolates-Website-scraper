bfb97800ed07776b8285c47a3ee371448397f9ff
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-28 22:30:59 +0200

    product url discovery scraping

---
+++ scrapyAmazonProducts/basic-scrapy-project/basic_scrapy_spider/spiders/quotes.py
@@ -0,0 +1,35 @@
+from collections.abc import Iterable
+import json
+import scrapy
+from urllib.parse import urlencode, urljoin
+import re
+
+class AmazonSearchProductSpider(scrapy.Spider):
+    name = "amazon_search_product"
+
+    def start_requests(self): 
+        keyword_list = ['ipad']
+        for keyword in keyword_list:
+            amazon_search_url = f'https://www.amazon.com/s?k={keyword}&page=1'
+            yield scrapy.Request(url=amazon_search_url, callback=self.discovery_product_urls, meta={'keyword': 'keyword', 'page': 1})
+
+    def discovery_product_urls(self, response):
+        page =  response.meta['page']
+        keyword = response.meta['keyword']
+
+        search_products = response.css('div.s-result-item[data-component-type=s-search-result]')
+        for product in search_products:
+            relative_url = product.css('h2>a::attr(href)').get()
+            product_url = urljoin("https://www.amazon.com/", relative_url).split('?')[0]
+            yield scrapy.Request(url=product_url, callback=self.parse_product_data, meta={'keyword': keyword, 'page': page})
+
+        # get all pages 
+        if page == 1:
+            available_pages = response.xpath('//*[contains(@class, "s-pagination-item")][not(has-class("s-pagination-separator"))]/text()').getall()
+
+            last_page = available_pages[-1]
+            for page_num in range(2, int(available_pages)):
+                amazon_search_url = f'https://www.amazon.com/s?k{keyword}&page={page_num}'
+                yield scrapy.Request(url=amazon_search_url, callback=self.parse_product_data, meta={'keyword': keyword, 'page': page_num})
+                
+
