eb891d84b289b47eb4d1aad31397a4edf45da1fe
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-30 18:06:38 +0200

    profile scraping - fetching and parsing of data

--- scrapyLinkedin/basic-scrapy-project/basic_scrapy_spider/spiders/quotes.py
+++ scrapyLinkedin/basic-scrapy-project/basic_scrapy_spider/spiders/quotes.py
@@ -23,3 +23,34 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
         summary_box = response.css('section.top-card-layout')
         item['name'] = summary_box.css('h1::text').get().strip()
         item['description'] = summary_box.css('h2::text').get().strip()
+
+        # location
+
+        try: 
+            item['location'] = summary_box.css('div.top-card__subline-item::text').get()
+        except: 
+            item['location'] = summary_box.css('span.top-card__subline-item::text').get().strip()
+            if 'followers' in item['location'] or 'connections' in item['location']:
+                item['location'] = ' '
+
+        item['followers'] = ''
+        item['connections'] = ''
+
+        for span_text in summary_box.css('span.top-card__subliner-item::text').getall():
+            if 'followers' in span_text:
+                item['followers'] = span_text.replace('followers', '').strip()
+            if 'connections' in span_text:
+                item['connections'] = span_text.replace('connections', '').strip()
+
+
+        """
+        ABOUT SECTION
+        """
+        item['about'] = response.css('section.summary div.core-section-container__content p::text').get(default='')
+
+        '''
+        EXPERIENCE SECTION
+        '''
+
+        item['experience'] = []
+        experience_block = response.css('li.experience-item')
